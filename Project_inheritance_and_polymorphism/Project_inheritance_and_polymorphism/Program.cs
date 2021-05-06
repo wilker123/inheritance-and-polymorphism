cbd7f1919ff8fa1d6831ebd3afa8cf518849f4a8
José Wilker Costa <47252771+wilker123@users.noreply.github.com>
2021-05-06 18:10:00 -0300

    Created the project with the solution

---
+++ Project_inheritance_and_polymorphism/Project_inheritance_and_polymorphism/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_inheritance_and_polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
