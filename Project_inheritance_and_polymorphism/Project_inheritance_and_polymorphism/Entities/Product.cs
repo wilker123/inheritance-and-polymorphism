8f9dc3e40680442bebd4268bff9cf1da9fb38ea8
José Wilker Costa <47252771+wilker123@users.noreply.github.com>
2021-05-06 19:42:12 -0300

    created the Product entity

---
+++ Project_inheritance_and_polymorphism/Project_inheritance_and_polymorphism/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_inheritance_and_polymorphism.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product() { }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string PriceTag()
+        {
+            return Name + " $" + Price;
+        }
+
+    }
+}
--- Project_inheritance_and_polymorphism/Project_inheritance_and_polymorphism/Program.cs
+++ Project_inheritance_and_polymorphism/Project_inheritance_and_polymorphism/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project_inheritance_and_polymorphism.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace Project_inheritance_and_polymorphism
     {
         static void Main(string[] args)
         {
+           
         }
     }
 }
